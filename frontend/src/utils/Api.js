4b67909a906eb8771f155d882f7344559793701d
Luna Hogart <moontochka@gmail.com>
2022-09-26 15:03:16 +0500

    fix: api connection

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -105,7 +105,7 @@ class Api {
 
 export const api = new Api(
   {
-    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-40',
+    baseUrl: 'https://api.gogetyourknife.nomorepartiesxyz.ru',
     headers: {
       authorization: `Bearer ${localStorage.getItem('token')}`,
       "Content-Type": "application/json",
